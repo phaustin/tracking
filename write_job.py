e2809efc28e9c5a429bdee5bebd1d7e5aebb4792
Phil Austin <paustin@eos.ubc.ca>
2017-06-04 09:24:54 -0700

    checkpoint

--- write_job.py
+++ write_job.py
@@ -16,13 +16,13 @@ type_id={"condensed": 0,
          "core_shell": 7,
          "plume": 8}
 
-def convert_file(the_file):
+def convert_file(case_dir,the_file):
     the_time=timestep.match(the_file).group(1)
     int_time=int(the_time)
     with h5py.File(the_file) as h5file:
         keep_recs=[]
         for cloud_id in h5file.keys():
-            print(f'full: {the_time}:{cloud_id}')
+            #print(f'full: {the_time}:{cloud_id}')
             cloud_num=int(cloud_id)
             if cloud_num == -1:
                 continue
@@ -30,14 +30,16 @@ def convert_file(the_file):
                 type_num=type_id[the_type]
                 for coord in h5file[cloud_id][the_type]:
                     keep_recs.append((cloud_num,type_num,int_time,coord))
-        pq_name=f'clouds_{the_time}.pq'
+        pq_name=f'{case_dir}/clouds_{the_time}.pq'
         df=pd.DataFrame.from_records(keep_recs,columns=['cloud_id','type','time_step','coord'])
         table = pa.Table.from_pandas(df)
-        print(f'{pq_name}')
+        print(f'wrote {pq_name}')
         pq.write_table(table, pq_name,compression='snappy')
 
 timestep=re.compile('.*_(\d+)\.h5')
-the_files=sorted(glob.glob('*h5'))[0:2]
-fun_list=[(convert_file,(the_file,),{}) for the_file in the_files]
-Parallel(n_jobs=4)(fun_list)
+case_dir='cgils_301'
+in_pat=f'{case_dir}/*h5'
+the_files=sorted(glob.glob(in_pat))
+fun_list=[(convert_file,(case_dir,the_file),{}) for the_file in the_files]
+Parallel(n_jobs=25)(fun_list)
 
